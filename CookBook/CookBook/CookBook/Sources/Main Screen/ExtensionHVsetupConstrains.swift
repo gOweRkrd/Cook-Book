5c6ccada29a0b67a509f0813e725d6ca5b4a38a4
Alexander Kosyakov <110721351+gOweRkrd@users.noreply.github.com>
2022-12-13 00:00:05 +0400

    correct

--- CookBook/CookBook/CookBook/Sources/Main Screen/ExtensionHVsetupConstrains.swift
+++ CookBook/CookBook/CookBook/Sources/Main Screen/ExtensionHVsetupConstrains.swift
@@ -24,36 +24,36 @@ extension HeaderView {
         NSLayoutConstraint.activate([
             americanButton.topAnchor.constraint(equalTo: welcomeTitle.topAnchor, constant:.americanButtonTopAnchor),
             americanButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: .americanButtonLeadingAnchor),
-            americanButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: .americanButtonTrailingAnchor),
+            americanButton.trailingAnchor.constraint(equalTo:japaneseButton.trailingAnchor,constant:.americanButtonTrailingAnchor),
             americanButton.heightAnchor.constraint(equalToConstant: .americanButtonHeightAnchor)])
         
         NSLayoutConstraint.activate([
-            italianButton.topAnchor.constraint(equalTo: americanButton.topAnchor, constant:.italianButtonTopAnchor),
-            italianButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: .italianButtonLeadingAnchor),
-            italianButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: .italianButtonTrailingAnchor),
-            italianButton.heightAnchor.constraint(equalToConstant: .italianButtonHeightAnchor)])
-        
-        NSLayoutConstraint.activate([
-            koreanButton.topAnchor.constraint(equalTo: japaneseButton.topAnchor, constant:.koreanButtonTopAnchor),
-            koreanButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: .koreanButtonTopAnchorButtonLeadingAnchor),
-            koreanButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: .koreanButtonTopAnchorButtonTrailingAnchor),
-            koreanButton.heightAnchor.constraint(equalToConstant: .koreanButtonTopAnchorButtonHeightAnchor)])
+            japaneseButton.topAnchor.constraint(equalTo: welcomeTitle.topAnchor, constant:.japaneseButtonTopAnchor),
+            japaneseButton.leadingAnchor.constraint(equalTo:americanButton.trailingAnchor,constant:.japeneseButtonLeadingAnchor),
+            japaneseButton.trailingAnchor.constraint(equalTo: germanButton.trailingAnchor,constant:.japaneseButtonTrailingAnchor),
+            japaneseButton.heightAnchor.constraint(equalToConstant: .japaneseButtonHeightAnchor)])
         
         NSLayoutConstraint.activate([
             germanButton.topAnchor.constraint(equalTo: welcomeTitle.topAnchor, constant:.germanButtonTopAnchor),
-            germanButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: .germanButtonLeadingAnchor),
+            germanButton.leadingAnchor.constraint(equalTo: japaneseButton.trailingAnchor,constant:.japeneseButtonLeadingAnchor),
             germanButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: .germanButtonTrailingAnchor),
             germanButton.heightAnchor.constraint(equalToConstant: .germanButtonHeightAnchor)])
         
         NSLayoutConstraint.activate([
-            japaneseButton.topAnchor.constraint(equalTo: welcomeTitle.topAnchor, constant:.japaneseButtonTopAnchor),
-            japaneseButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: .japeneseButtonLeadingAnchor),
-            japaneseButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: .japaneseButtonTrailingAnchor),
-            japaneseButton.heightAnchor.constraint(equalToConstant: .japaneseButtonHeightAnchor)])
+            italianButton.topAnchor.constraint(equalTo: americanButton.topAnchor, constant:.italianButtonTopAnchor),
+            italianButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: .italianButtonLeadingAnchor),
+            italianButton.trailingAnchor.constraint(equalTo: koreanButton.trailingAnchor,constant:.italianButtonTrailingAnchor),
+            italianButton.heightAnchor.constraint(equalToConstant: .italianButtonHeightAnchor)])
         
         NSLayoutConstraint.activate([
+            koreanButton.topAnchor.constraint(equalTo: japaneseButton.topAnchor, constant:.koreanButtonTopAnchor),
+            koreanButton.leadingAnchor.constraint(equalTo:italianButton.trailingAnchor,constant:.koreanButtonTopAnchorButtonLeadingAnchor ),
+            koreanButton.trailingAnchor.constraint(equalTo: mexicanButton.trailingAnchor,constant:.koreanButtonTopAnchorButtonTrailingAnchor),
+            koreanButton.heightAnchor.constraint(equalToConstant: .koreanButtonTopAnchorButtonHeightAnchor)])
+    
+        NSLayoutConstraint.activate([
             mexicanButton.topAnchor.constraint(equalTo: japaneseButton.topAnchor, constant:.mexicanButtonTopAnchor),
-            mexicanButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: .mexicanButtonLeadingAnchor),
+            mexicanButton.leadingAnchor.constraint(equalTo:koreanButton.trailingAnchor,constant:.mexicanButtonLeadingAnchor) ,
             mexicanButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: .mexicanButtonTrailingAnchor),
             mexicanButton.heightAnchor.constraint(equalToConstant: .mexicanButtonHeightAnchor)])
         
@@ -72,31 +72,31 @@ private extension CGFloat {
     
     static let americanButtonTopAnchor: CGFloat = 70
     static let americanButtonLeadingAnchor: CGFloat = 10
-    static let americanButtonTrailingAnchor: CGFloat = -265
+    static let americanButtonTrailingAnchor: CGFloat = -125
     static let americanButtonHeightAnchor : CGFloat = 50
     
-    static let italianButtonTopAnchor: CGFloat = 60
-    static let italianButtonLeadingAnchor: CGFloat = 10
-    static let italianButtonTrailingAnchor: CGFloat = -265
-    static let italianButtonHeightAnchor : CGFloat = 50
-    
     static let japaneseButtonTopAnchor: CGFloat = 70
-    static let japeneseButtonLeadingAnchor: CGFloat = 135
-    static let japaneseButtonTrailingAnchor: CGFloat = -140
+    static let japeneseButtonLeadingAnchor: CGFloat = 5
+    static let japaneseButtonTrailingAnchor: CGFloat = -125
     static let japaneseButtonHeightAnchor : CGFloat = 50
     
-    static let koreanButtonTopAnchor: CGFloat = 60
-    static let koreanButtonTopAnchorButtonLeadingAnchor: CGFloat = 135
-    static let koreanButtonTopAnchorButtonTrailingAnchor: CGFloat = -140
-    static let koreanButtonTopAnchorButtonHeightAnchor : CGFloat = 50
-    
     static let germanButtonTopAnchor: CGFloat = 70
-    static let germanButtonLeadingAnchor: CGFloat = 260
+    static let germanButtonLeadingAnchor: CGFloat = 5
     static let germanButtonTrailingAnchor: CGFloat = -10
     static let germanButtonHeightAnchor : CGFloat = 50
     
+    static let italianButtonTopAnchor: CGFloat = 60
+    static let italianButtonLeadingAnchor: CGFloat = 10
+    static let italianButtonTrailingAnchor: CGFloat = -125
+    static let italianButtonHeightAnchor : CGFloat = 50
+    
+    static let koreanButtonTopAnchor: CGFloat = 60
+    static let koreanButtonTopAnchorButtonLeadingAnchor: CGFloat = 5
+    static let koreanButtonTopAnchorButtonTrailingAnchor: CGFloat = -125
+    static let koreanButtonTopAnchorButtonHeightAnchor : CGFloat = 50
+    
     static let mexicanButtonTopAnchor: CGFloat = 60
-    static let mexicanButtonLeadingAnchor: CGFloat = 260
+    static let mexicanButtonLeadingAnchor: CGFloat = 5
     static let mexicanButtonTrailingAnchor: CGFloat = -10
     static let mexicanButtonHeightAnchor : CGFloat = 50
     
